84f7f3ef508d8008d554aea21b7fa02f8663ff66
weeiner <71634275+weeiner@users.noreply.github.com>
2025-03-02 14:00:27 +0100

    added hello world query

--- problem-5/hello/x/hello/keeper/query_say_hello.go
+++ problem-5/hello/x/hello/keeper/query_say_hello.go
@@ -1,24 +1,25 @@
 package keeper
 
 import (
-	"context"
+    "context"
+    "fmt"
 
-	"hello/x/hello/types"
+    "hello/x/hello/types"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+    sdk "github.com/cosmos/cosmos-sdk/types"
+    "google.golang.org/grpc/codes"
+    "google.golang.org/grpc/status"
 )
 
-func (k Keeper) SayHello(goCtx context.Context, req *types.QuerySayHelloRequest) (*types.QuerySayHelloResponse, error) {
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid request")
-	}
+func (k Keeper) SayHello(ctx context.Context, req *types.QuerySayHelloRequest) (*types.QuerySayHelloResponse, error) {
+    if req == nil {
+        return nil, status.Error(codes.InvalidArgument, "invalid request")
+    }
 
-	ctx := sdk.UnwrapSDKContext(goCtx)
+    // Validation and Context unwrapping
+    sdkCtx := sdk.UnwrapSDKContext(ctx)
 
-	// TODO: Process the query
-	_ = ctx
-
-	return &types.QuerySayHelloResponse{}, nil
+    _ = sdkCtx
+    // Custom Response
+    return &types.QuerySayHelloResponse{Name: fmt.Sprintf("Hello %s!", req.Name)}, nil
 }
