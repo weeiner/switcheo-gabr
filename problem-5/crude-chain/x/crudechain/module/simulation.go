2b887d1a133284bef636c40af3cdf3cc84f4c7a2
Developer Experience team at Ignite <hello@ignite.com>
2025-03-02 14:41:21 +0100

    Initialized with Ignite CLI

---
+++ problem-5/crude-chain/x/crudechain/module/simulation.go
@@ -0,0 +1,59 @@
+package crudechain
+
+import (
+	"math/rand"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+
+	"crude-chain/testutil/sample"
+	crudechainsimulation "crude-chain/x/crudechain/simulation"
+	"crude-chain/x/crudechain/types"
+)
+
+// avoid unused import issue
+var (
+	_ = crudechainsimulation.FindAccount
+	_ = rand.Rand{}
+	_ = sample.AccAddress
+	_ = sdk.AccAddress{}
+	_ = simulation.MsgEntryKind
+)
+
+const (
+// this line is used by starport scaffolding # simapp/module/const
+)
+
+// GenerateGenesisState creates a randomized GenState of the module.
+func (AppModule) GenerateGenesisState(simState *module.SimulationState) {
+	accs := make([]string, len(simState.Accounts))
+	for i, acc := range simState.Accounts {
+		accs[i] = acc.Address.String()
+	}
+	crudechainGenesis := types.GenesisState{
+		Params: types.DefaultParams(),
+		// this line is used by starport scaffolding # simapp/module/genesisState
+	}
+	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(&crudechainGenesis)
+}
+
+// RegisterStoreDecoder registers a decoder.
+func (am AppModule) RegisterStoreDecoder(_ simtypes.StoreDecoderRegistry) {}
+
+// WeightedOperations returns the all the gov module operations with their respective weights.
+func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
+	operations := make([]simtypes.WeightedOperation, 0)
+
+	// this line is used by starport scaffolding # simapp/module/operation
+
+	return operations
+}
+
+// ProposalMsgs returns msgs used for governance proposals for simulations.
+func (am AppModule) ProposalMsgs(simState module.SimulationState) []simtypes.WeightedProposalMsg {
+	return []simtypes.WeightedProposalMsg{
+		// this line is used by starport scaffolding # simapp/module/OpMsg
+	}
+}
---
+++ problem-5/crude-chain/x/crudechain/types/keys.go
@@ -0,0 +1,20 @@
+package types
+
+const (
+	// ModuleName defines the module name
+	ModuleName = "crudechain"
+
+	// StoreKey defines the primary module store key
+	StoreKey = ModuleName
+
+	// MemStoreKey defines the in-memory store key
+	MemStoreKey = "mem_crudechain"
+)
+
+var (
+	ParamsKey = []byte("p_crudechain")
+)
+
+func KeyPrefix(p string) []byte {
+	return []byte(p)
+}
