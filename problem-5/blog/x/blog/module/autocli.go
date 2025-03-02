e27a829e523912d09ac8f9ae16cfe03cb1ef8b88
weeiner <71634275+weeiner@users.noreply.github.com>
2025-03-02 14:08:37 +0100

    allow users to create posts

--- problem-5/blog/x/blog/module/autocli.go
+++ problem-5/blog/x/blog/module/autocli.go
@@ -28,6 +28,12 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					RpcMethod: "UpdateParams",
 					Skip:      true, // skipped because authority gated
 				},
+				{
+					RpcMethod:      "CreatePost",
+					Use:            "create-post [title] [body]",
+					Short:          "Send a create-post tx",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "title"}, {ProtoField: "body"}},
+				},
 				// this line is used by ignite scaffolding # autocli/tx
 			},
 		},
