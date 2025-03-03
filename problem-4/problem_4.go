d7968c24f47fa887cda4b53957f72c40aa6d2f33
weeiner <71634275+weeiner@users.noreply.github.com>
2025-03-03 02:58:08 +0100

    added comments on complexity & efficiency for problem 4

--- problem-4/problem_4.go
+++ problem-4/problem_4.go
@@ -2,12 +2,15 @@ package main
 
 func sum_to_n_a(n int) int {
 	// your code here
-	sum := 0;
+	sum := 0
 	for i := 1; i <= n; i++ {
 		sum += i
 	}
-	return sum;
+	return sum
 }
+
+// time complexity : o(n) -> loop iterates n times
+// efficiency : moderate. Works well for moderate values of n. Inefficient for very large values of n
 
 func sum_to_n_b(n int) int {
 	// your code here
@@ -16,16 +19,21 @@ func sum_to_n_b(n int) int {
 	return x
 }
 
+// time complexity: O(1) (constant) – fixed number of operations regardless of n
+// efficiency: very efficient. performs the same for large n
+
 func sum_to_n_c(n int) int {
 	// your code here
-	// recursion 
-	// base case 
-	if n ==	0 {
+	// recursion
+	// base case
+	if n == 0 {
 		return 0
 	}
-	return n + sum_to_n_c(n - 1)
+	return n + sum_to_n_c(n-1)
 }
 
+// time complexity: O(n) – Makes n recursive calls
+// efficiency: not efficient for large n. As it utilizes recursion, can result in stack overflow when n is large
 
 func main() {
 	test1 := sum_to_n_a(10)
@@ -36,39 +44,3 @@ func main() {
 	println(test2)
 	println(test3)
 }
-
-
-
-// alternative solution
-// using go routine -> staring a new thread in Java
-// make use of channel when doing 
-
-// make more base cases, then call 
-
-// alternative 
-// func sum_to_n_d(n int) int {
-// 	subSum1 := make(chan int)
-// 	subSum2 := make(chan int)
-// 	subSum3 := make(chan int)
-   
-// 	// Function to compute partial sums in goroutines
-// 	partSum := func(start, end int, subCh chan int) {
-// 	 sum := 0
-// 	 for num := start; num <= end; num++ {
-// 	  sum += num
-// 	 }
-// 	 subCh <- sum // Send the sum to the channel
-// 	}
-   
-// 	// Divide n into 3 parts and compute sums concurrently
-// 	go partSum(1, n/3, subSum1)
-// 	go partSum(n/3+1, 2*n/3, subSum2)
-// 	go partSum(2*n/3+1, n, subSum3)
-   
-// 	// Collect results from channels
-// 	result := <-subSum1 + <-subSum2 + <-subSum3
-// 	return result
-//    }
-
-
-// sumsum1, subsum2, subsum3 must execute finish in order to get a result. else it will wait indefinitely
